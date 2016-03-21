01d0ce79c0f5af0e1fb760670ffecb55d037fbd4
Pedro Lacerda <pedrohrl1@gmail.com>
2016-03-21 18:33:06 +0100

    New dataformat for the heatmap
    Heatmap using JSON

--- Dashboard/js/map.js
+++ Dashboard/js/map.js
@@ -53,16 +53,7 @@ d3.json("data/amsterdam.geojson", function(collection) {		// was: groningen.geoj
              return color(Math.random() * 100);				// was: P_EENP_HH
         })
         .attr("class", "unselected")
-        .style("fill-opacity", "0.7")
-        .append("title");
-
-    feature
-        .select("title")
-        .text(function(d) {
-            // geef iedere buurt een titel met de buurtnaam en het percentage eenpersoonshuishoudens
-            return d.properties.BU_CODE + ": " + d.properties.BU_NAME.toString();		//was: BU_NAAM 	//was: P_EENP_HH
-			//return d.properties.BCNAAM;		//was: BU_NAAM 	//was: P_EENP_HH
-        });
+        .style("fill-opacity", "0.7");
 
     feature
         .on("click", function(d){
